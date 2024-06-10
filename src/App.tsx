10d5b58a5d9d06ec4d28ebdb6b429661e041b813
Jason Rudin <j.b.rudin@gmail.com>
2024-06-10 18:07:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="w-1/2 h-1/2 bg-gray-100 rounded-lg shadow-lg flex items-center justify-center font-medium text-xl">
-        Your To-Do app goes here!
+        Your To-Do app goes here! 🚀 
       </div>
     </div>
   );
